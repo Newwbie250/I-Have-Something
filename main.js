0963def5dca491f62362dc1a4ae5854fa91370b5
Newwbie250 <asadnurfajri@gmail.com>
2024-11-30 18:19:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('For U Suci 💐').split('')
+    const titles = ('For You Suci 💐').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
